deefcceb8a48ed4a51703d88e446c23d13c32f25
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-03 14:18:06 -0600

    reordered

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ let automobiles= ['car', 'truck', 'motorcycle']
 automobiles.push('airplane', 'skateboard');
 document.getElementById("push").innerHTML = automobiles;
 
-automobiles.unshift('helicopter');
-document.getElementById("unshift").innerHTML = automobiles;
-
-let automobilesCopy= ['automobiles']
-
 automobiles.pop();
 document.getElementById("pop").innerHTML = automobiles;
+
+automobiles.unshift('helicopter');
+document.getElementById("unshift").innerHTML = automobiles;
 
 automobiles.shift();
 document.getElementById("shift").innerHTML = automobiles;
 
+let automobilesCopy= automobiles;
+
 document.getElementById("result").innerHTML = automobiles;
+
 
 
 
