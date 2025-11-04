59419ebd225304a0ad7238225d1224e320831843
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-04 12:04:02 -0600

    removed semi colon

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-let automobiles= ['car', 'truck', 'motorcycle'];
+let automobiles= ['car', 'truck', 'motorcycle']
 
 automobiles.push('airplane', 'skateboard');
 
-
 automobiles.pop();
-
 
 automobiles.unshift('helicopter', 'bike');
 
-
 automobiles.shift();
-
 
 let automobilesCopy= automobiles;
 
@@ -20,6 +16,7 @@ document.getElementById("result").innerHTML = automobiles;
       ✕ should add airplane and skateboard to the end of automobiles (139 ms)
       ✕ should add helicopter and bike to the front of the automobiles (168 ms)
       ✕ should define a new variable called automobilesCopy and set it equal to a string template that contains the variable automobiles (155 ms)
+
 
 
 
