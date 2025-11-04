1ff9ed3fbbcd0fea7a3940ac04e97f7ba6fe5c4a
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-04 12:08:09 -0600

    removed notes

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ let automobilesCopy= automobiles;
 
 document.getElementById("result").innerHTML = automobiles;
 
-    ✕ should create a variable named automobiles that contains car, truck, motorcycle (1000 ms)
-      ✕ should add airplane and skateboard to the end of automobiles (139 ms)
-      ✕ should add helicopter and bike to the front of the automobiles (168 ms)
-      ✕ should define a new variable called automobilesCopy and set it equal to a string template that contains the variable automobiles (155 ms)
+
+
 
 
 
