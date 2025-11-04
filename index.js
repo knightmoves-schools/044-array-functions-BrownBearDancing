f35f6ff583dcc46bf33c39511201ef4505df9032
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-04 11:43:07 -0600

    removed extra get element id

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 let automobiles= ['car', 'truck', 'motorcycle']
 
 automobiles.push('airplane', 'skateboard');
-document.getElementById("push").innerHTML = automobiles;
+
 
 automobiles.pop();
-document.getElementById("pop").innerHTML = automobiles;
+
 
 automobiles.unshift('helicopter');
-document.getElementById("unshift").innerHTML = automobiles;
+
 
 automobiles.shift();
-document.getElementById("shift").innerHTML = automobiles;
+
 
 let automobilesCopy= automobiles;
 
 document.getElementById("result").innerHTML = automobiles;
+
 
 
 
