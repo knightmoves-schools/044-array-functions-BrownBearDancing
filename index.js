6313256f1433c99a4a2a9d400c32a8a2b429535f
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-04 11:48:11 -0600

    changed to var

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let automobiles= ['car', 'truck', 'motorcycle']
+var automobiles= ['car', 'truck', 'motorcycle']
 
 automobiles.push('airplane', 'skateboard');
 
@@ -6,7 +6,7 @@ automobiles.push('airplane', 'skateboard');
 automobiles.pop();
 
 
-automobiles.unshift('helicopter');
+automobiles.unshift('helicopter', 'bike');
 
 
 automobiles.shift();
@@ -16,6 +16,10 @@ let automobilesCopy= automobiles;
 
 document.getElementById("result").innerHTML = automobiles;
 
+    ✕ should create a variable named automobiles that contains car, truck, motorcycle (1000 ms)
+      ✕ should add airplane and skateboard to the end of automobiles (139 ms)
+      ✕ should add helicopter and bike to the front of the automobiles (168 ms)
+      ✕ should define a new variable called automobilesCopy and set it equal to a string template that contains the variable automobiles (155 ms)
 
 
 
